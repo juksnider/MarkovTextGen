3ab7e9b9cd64265e6d75eb757b981e588ce57ae8
Jack Snyder <jacksnyder@Andies-MacBook-Air.local>
2019-04-24 13:46:48 -0400

    made the code slightly less sloppy

--- markov.py
+++ markov.py
@@ -1,27 +1,30 @@
+#Importing random library
 import random as r
-#Markov chain program by JukSnider
+#Markov chain program by Jack Snyder (JukSnider)
 r.seed()
-#
+#Opens text file, creates list of all words split at spaces
 inputfile = open("input.txt","r")
 text = inputfile.read()
-dumlist = text.split()
-fulltextlist = []
+#List of all text
+fulltextlist = text.split()
+#List of unique words
 unique = []
-for x in dumlist:
-    fulltextlist.append(x)
+#Fulltextlist and unique ar intiallized
+for x in fulltextlist:
     if x not in unique:
         unique.append(x)
-numwords = len(unique)
+#Markov[] is a list of lists that forms an array of probability for words
+#Each list in markov directly coresponds to the word of the same index in unique[]. Each word in that list is a word that came after the corresponding word in unique
 markov = []
-dumlisttwo = fulltextlist
 for x in unique:
     markov.insert(0, [])
-#Test
+#Initalizing Markov and making fulltextlist into
 for x in fulltextlist:
     ind = unique.index(x)
     if fulltextlist.index(x)+1 < len(fulltextlist):
         markov[ind].insert(1, fulltextlist[fulltextlist.index(x)+1])
     fulltextlist[fulltextlist.index(x)] = "okaythisisepic"
+#Prompt for how many words to be generated
 words = input("How many words do you want to have? >>>")
 words = int(words)-1
 startingwordone = markov[r.randint(0,len(markov))-1]
